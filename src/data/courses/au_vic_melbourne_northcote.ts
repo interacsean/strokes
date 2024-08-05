06b307abe440acaddee74defd45afa420d2367b5
s720788 <sean.hasselback@iag.com.au>
2024-08-05 17:04:32 +1000

    feat: Add Nortcote course data

---
+++ src/data/courses/au_vic_melbourne_northcote.ts
@@ -0,0 +1,124 @@
+import { CourseDef } from "model/Course";
+
+export const auVicMelbourneNorthcote: CourseDef = {
+  courseName: "Northcote",
+  holes: [
+    {
+      holeNum: 1,
+      tees: {
+        tee: {
+          par: 4,
+          nominalDistance: 287,
+          pos: { lat: -37.758007544374024, lng: 144.98399686860003, alt: null },
+        },
+      },
+      pins: {
+        pin: { lat: -37.757777760867, lng: 144.98094538996065, alt: null },
+      },
+    },
+    {
+      holeNum: 2,
+      tees: {
+        tee: {
+          par: 4,
+          nominalDistance: 416,
+          pos: { lat: -37.75810011283434, lng: 144.98085562208644, alt: null },
+        },
+      },
+      pins: {
+        pin: { lat: -37.760913604090135, lng: 144.98305931322457, alt: null },
+      },
+    },
+    {
+      holeNum: 3,
+      tees: {
+        tee: {
+          par: 3,
+          nominalDistance: 167,
+          pos: { lat: -37.761052724993746, lng: 144.98357187335128, alt: null },
+        },
+      },
+      pins: {
+        pin: { lat: -37.759662274502375, lng: 144.98295738854205, alt: null },
+      },
+    },
+    {
+      holeNum: 4,
+      tees: {
+        tee: {
+          par: 4,
+          nominalDistance: 378,
+          pos: { lat: -37.76024413110629, lng: 144.98381497631004, alt: null },
+        },
+      },
+      pins: {
+        pin: { lat: -37.76259967584733, lng: 144.9823482290039, alt: null },
+      },
+    },
+    {
+      holeNum: 5,
+      tees: {
+        tee: {
+          par: 3,
+          nominalDistance: 101,
+          pos: { lat: -37.76280906596082, lng: 144.98319455659396, alt: null },
+        },
+      },
+      pins: {
+        pin: { lat: -37.76235932970707, lng: 144.9841924153016, alt: null },
+      },
+    },
+    {
+      holeNum: 6,
+      tees: {
+        tee: {
+          par: 3,
+          nominalDistance: 128,
+          pos: { lat: -37.76343462250494, lng: 144.9852974124151, alt: null },
+        },
+      },
+      pins: {
+        pin: { lat: -37.76289464517224, lng: 144.98411771631743, alt: null },
+      },
+    },
+    {
+      holeNum: 7,
+      tees: {
+        tee: {
+          par: 4,
+          nominalDistance: 226,
+          pos: { lat: -37.76235279361025, lng: 144.98479427464073, alt: null },
+        },
+      },
+      pins: {
+        pin: { lat: -37.76050182246079, lng: 144.98449093760604, alt: null },
+      },
+    },
+    {
+      holeNum: 8,
+      tees: {
+        tee: {
+          par: 5,
+          nominalDistance: 466,
+          pos: { lat: -37.75988736142931, lng: 144.9864227800182, alt: null },
+        },
+      },
+      pins: {
+        pin: { lat: -37.75864205149709, lng: 144.98205442005357, alt: null },
+      },
+    },
+    {
+      holeNum: 9,
+      tees: {
+        tee: {
+          par: 4,
+          nominalDistance: 303,
+          pos: { lat: -37.75829400788681, lng: 144.98185273848316, alt: null },
+        },
+      },
+      pins: {
+        pin: { lat: -37.75923935587282, lng: 144.98469229013253, alt: null },
+      },
+    },
+  ],
+};
