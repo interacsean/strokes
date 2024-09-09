94ae94d278b298ab18e579ab29344e7cd1c6bf9c
s720788 <sean.hasselback@iag.com.au>
2024-09-10 09:52:24 +1000

    chore: Update some example clubStats

--- src/state/rounds/selectors/clubStats.ts
+++ src/state/rounds/selectors/clubStats.ts
@@ -9,9 +9,9 @@ export const selectClubStats = (state: RoundsState): ClubStats => {
   return {
     [Club.D]: {
       [StrokeType.FULL]: {
-        medianDistance: 190,
-        sd1Distances: [160, 203] as [number, number],
-        sd2Distances: [132, 210] as [number, number],
+        medianDistance: 180,
+        sd1Distances: [170, 195] as [number, number],
+        sd2Distances: [160, 210] as [number, number],
         sd1Side: [-3, 17] as [number, number],
         sd2Side: [10, 25] as [number, number],
         strikeQuality: {
@@ -23,11 +23,27 @@ export const selectClubStats = (state: RoundsState): ClubStats => {
         },
       },
     },
-    [Club["4H"]]: {
+    [Club["3W"]]: {
       [StrokeType.FULL]: {
         medianDistance: 165,
-        sd1Distances: [150, 170] as [number, number],
-        sd2Distances: [135, 174] as [number, number],
+        sd1Distances: [160, 170] as [number, number],
+        sd2Distances: [135, 175] as [number, number],
+        sd1Side: [4, 7] as [number, number],
+        sd2Side: [6, 12] as [number, number],
+        strikeQuality: {
+          CLEAN: 0.5,
+          SLICE: 0.3,
+          TOPPED: 0.05,
+          SKYBALL: 0.03,
+          HOOK: 0.12,
+        },
+      },
+    },
+    [Club["4H"]]: {
+      [StrokeType.FULL]: {
+        medianDistance: 155,
+        sd1Distances: [150, 160] as [number, number],
+        sd2Distances: [135, 165] as [number, number],
         sd1Side: [5, 6] as [number, number],
         sd2Side: [6.5, 8] as [number, number],
         strikeQuality: {
@@ -39,9 +55,25 @@ export const selectClubStats = (state: RoundsState): ClubStats => {
         },
       },
     },
+    [Club["5I"]]: {
+      [StrokeType.FULL]: {
+        medianDistance: 140,
+        sd1Distances: [130, 150] as [number, number],
+        sd2Distances: [130, 155] as [number, number],
+        sd1Side: [5, 5] as [number, number],
+        sd2Side: [7, 7] as [number, number],
+        strikeQuality: {
+          CLEAN: 0.65,
+          SLICE: 0.15,
+          HOOK: 0.05,
+          THIN: 0.1,
+          THICK: 0.05,
+        },
+      },
+    },
     [Club["6I"]]: {
       [StrokeType.FULL]: {
-        medianDistance: 135,
+        medianDistance: 130,
         sd1Distances: [120, 140] as [number, number],
         sd2Distances: [110, 145] as [number, number],
         sd1Side: [5, 5] as [number, number],
@@ -57,7 +89,7 @@ export const selectClubStats = (state: RoundsState): ClubStats => {
     },
     [Club["7I"]]: {
       [StrokeType.FULL]: {
-        medianDistance: 125,
+        medianDistance: 120,
         sd1Distances: [110, 130] as [number, number],
         sd2Distances: [100, 135] as [number, number],
         sd1Side: [5, 5] as [number, number],
@@ -73,8 +105,8 @@ export const selectClubStats = (state: RoundsState): ClubStats => {
     },
     [Club["8I"]]: {
       [StrokeType.FULL]: {
-        medianDistance: 115,
-        sd1Distances: [100, 120] as [number, number],
+        medianDistance: 110,
+        sd1Distances: [105, 115] as [number, number],
         sd2Distances: [90, 125] as [number, number],
         sd1Side: [4, 4] as [number, number],
         sd2Side: [5, 5] as [number, number],
@@ -87,10 +119,20 @@ export const selectClubStats = (state: RoundsState): ClubStats => {
         },
       },
     },
+    [Club["9I"]]: {
+      [StrokeType.FULL]: {
+        medianDistance: 101,
+        sd1Distances: [96, 106] as [number, number],
+        sd2Distances: [96, 106] as [number, number],
+        sd1Side: [4, 4] as [number, number],
+        sd2Side: [5, 5] as [number, number],
+        strikeQuality: {},
+      },
+    },
     [Club["PW"]]: {
       [StrokeType.FULL]: {
-        medianDistance: 100,
-        sd1Distances: [90, 105] as [number, number],
+        medianDistance: 90,
+        sd1Distances: [85, 95] as [number, number],
         sd2Distances: [85, 110] as [number, number],
         sd1Side: [3, 3] as [number, number],
         sd2Side: [4, 4] as [number, number],
@@ -119,9 +161,9 @@ export const selectClubStats = (state: RoundsState): ClubStats => {
     },
     [Club["SW"]]: {
       [StrokeType.FULL]: {
-        medianDistance: 85,
-        sd1Distances: [70, 91] as [number, number],
-        sd2Distances: [75, 105] as [number, number],
+        medianDistance: 75,
+        sd1Distances: [70, 80] as [number, number],
+        sd2Distances: [65, 82] as [number, number],
         sd1Side: [3, 3] as [number, number],
         sd2Side: [4, 4] as [number, number],
         strikeQuality: {
@@ -133,9 +175,9 @@ export const selectClubStats = (state: RoundsState): ClubStats => {
         },
       },
       [StrokeType.THREE_QTR]: {
-        medianDistance: 70,
-        sd1Distances: [65, 80] as [number, number],
-        sd2Distances: [55, 85] as [number, number],
+        medianDistance: 55,
+        sd1Distances: [50, 60] as [number, number],
+        sd2Distances: [45, 65] as [number, number],
         sd1Side: [2, 2] as [number, number],
         sd2Side: [3, 3] as [number, number],
         strikeQuality: {
