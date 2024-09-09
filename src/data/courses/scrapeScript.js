643aefaaf9fa37189c6450dd719c49bd3abe0708
s720788 <sean.hasselback@iag.com.au>
2024-09-10 09:51:37 +1000

    chore: Keep golftraxx position scraper

---
+++ src/data/courses/scrapeScript.js
@@ -0,0 +1,61 @@
+/**
+ * Function to scrape hole data from golftraxx hole-layout
+ */
+(function getCourseCoordinates() {
+  return {
+    holeNum: parseInt(
+      document
+        .querySelector(
+          "body > div > div > div.container-fluid.mid-container.pt-4.pb-4 > div:nth-child(2) > div > font:nth-child(2) > font"
+        )
+        .innerText.match(/Hole# (\d*)/i)[1],
+      10
+    ),
+    tees: {
+      tee: {
+        pos: {
+          ...Object.entries(savedVals).filter(
+            ([k, v]) => !["gc", "gf", "gb", "tt"].includes(k)
+          )[0][1],
+          alt: null,
+        },
+        nominalDistance: parseInt(
+          document
+            .querySelector(
+              "body > div > div > div.container-fluid.mid-container.pt-4.pb-4 > div:nth-child(2) > div > font:nth-child(2) > font"
+            )
+            .innerText.match(/YDG (\d*)/i)[1],
+          10
+        ),
+        par: parseInt(
+          document
+            .querySelector(
+              "body > div > div > div.container-fluid.mid-container.pt-4.pb-4 > div:nth-child(2) > div > font:nth-child(2) > font"
+            )
+            .innerText.match(/Par (\d*)/i)[1],
+          10
+        ),
+      },
+    },
+    pins: {
+      pin: {
+        lat: parseFloat(gclatitude),
+        lng: parseFloat(gclongitude),
+        alt: null,
+      },
+    },
+    green: {
+      back: {
+        lat: parseFloat(gblatitude),
+        lng: parseFloat(gblongitude),
+        alt: null,
+      },
+      front: {
+        lat: parseFloat(gflatitude),
+        lng: parseFloat(gflongitude),
+        alt: null,
+      },
+    },
+    waypoints: [savedVals.tt],
+  };
+})();
