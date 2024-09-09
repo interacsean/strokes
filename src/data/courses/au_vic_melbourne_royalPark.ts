0403675c4bc8b63b5658bc83de608f9e89079ad7
s720788 <sean.hasselback@iag.com.au>
2024-09-10 09:51:14 +1000

    feat: Add Westgate and Royal Park courses

---
+++ src/data/courses/au_vic_melbourne_royalPark.ts
@@ -0,0 +1,358 @@
+import { CourseDef } from "model/Course";
+
+export const auVicMelbourneRoyalPark: CourseDef = {
+  courseName: "Royal Park",
+  holes: [
+    {
+      holeNum: 1,
+      tees: {
+        tee: {
+          pos: {
+            lat: -37.78212138902201,
+            lng: 144.95617264009886,
+            alt: null,
+          },
+          nominalDistance: 182,
+          par: 3,
+        },
+      },
+      pins: {
+        pin: {
+          lat: -37.781962093759994,
+          lng: 144.9542800941054,
+          alt: null,
+        },
+      },
+      green: {
+        back: {
+          lat: -37.781967700773194,
+          lng: 144.95414818803445,
+          alt: null,
+        },
+        front: {
+          lat: -37.78196019707691,
+          lng: 144.95443190670346,
+          alt: null,
+        },
+      },
+      waypoints: [
+        {
+          lat: -37.78212138902201,
+          lng: 144.95617264009886,
+        },
+      ],
+    },
+    {
+      holeNum: 2,
+      tees: {
+        tee: {
+          pos: {
+            lat: -37.78070056489172,
+            lng: 144.95153864776114,
+            alt: null,
+          },
+          nominalDistance: 323,
+          par: 4,
+        },
+      },
+      pins: {
+        pin: {
+          lat: -37.780602543802814,
+          lng: 144.95489059873753,
+          alt: null,
+        },
+      },
+      green: {
+        back: {
+          lat: -37.78059761492813,
+          lng: 144.95501129602658,
+          alt: null,
+        },
+        front: {
+          lat: -37.78062231209603,
+          lng: 144.95477703935444,
+          alt: null,
+        },
+      },
+      waypoints: [
+        {
+          lat: -37.78063646247235,
+          lng: 144.9542644555579,
+        },
+      ],
+    },
+    {
+      holeNum: 3,
+      tees: {
+        tee: {
+          pos: {
+            lat: -37.780412822641956,
+            lng: 144.9543021878938,
+            alt: null,
+          },
+          nominalDistance: 247,
+          par: 4,
+        },
+      },
+      pins: {
+        pin: {
+          lat: -37.77891047538468,
+          lng: 144.9527005506308,
+          alt: null,
+        },
+      },
+      green: {
+        back: {
+          lat: -37.77886850357002,
+          lng: 144.9526034645515,
+          alt: null,
+        },
+        front: {
+          lat: -37.77895324243724,
+          lng: 144.952798230657,
+          alt: null,
+        },
+      },
+      waypoints: [
+        {
+          lat: -37.779101272065816,
+          lng: 144.95328101411403,
+        },
+      ],
+    },
+    {
+      holeNum: 4,
+      tees: {
+        tee: {
+          pos: {
+            lat: -37.77881071143642,
+            lng: 144.95233162912604,
+            alt: null,
+          },
+          nominalDistance: 172,
+          par: 3,
+        },
+      },
+      pins: {
+        pin: {
+          lat: -37.777712404512485,
+          lng: 144.95121548591658,
+          alt: null,
+        },
+      },
+      green: {
+        back: {
+          lat: -37.777654327326154,
+          lng: 144.95112753413346,
+          alt: null,
+        },
+        front: {
+          lat: -37.77776675791526,
+          lng: 144.95129797233645,
+          alt: null,
+        },
+      },
+      waypoints: [
+        {
+          lat: -37.77881479640619,
+          lng: 144.95232860265776,
+        },
+      ],
+    },
+    {
+      holeNum: 5,
+      tees: {
+        tee: {
+          pos: {
+            lat: -37.77785728847342,
+            lng: 144.9506681832731,
+            alt: null,
+          },
+          nominalDistance: 423,
+          par: 5,
+        },
+      },
+      pins: {
+        pin: {
+          lat: -37.780342553849636,
+          lng: 144.9537303549464,
+          alt: null,
+        },
+      },
+      green: {
+        back: {
+          lat: -37.78040030191998,
+          lng: 144.95380164291726,
+          alt: null,
+        },
+        front: {
+          lat: -37.78025702142855,
+          lng: 144.95366222242754,
+          alt: null,
+        },
+      },
+      waypoints: [
+        {
+          lat: -37.77938222168202,
+          lng: 144.9527551469951,
+        },
+      ],
+    },
+    {
+      holeNum: 6,
+      tees: {
+        tee: {
+          pos: {
+            lat: -37.780306410901325,
+            lng: 144.95293953805296,
+            alt: null,
+          },
+          nominalDistance: 158,
+          par: 3,
+        },
+      },
+      pins: {
+        pin: {
+          lat: -37.77950525463391,
+          lng: 144.9516437039716,
+          alt: null,
+        },
+      },
+      green: {
+        back: {
+          lat: -37.779452350778215,
+          lng: 144.95156176629445,
+          alt: null,
+        },
+        front: {
+          lat: -37.779565477063784,
+          lng: 144.95172509216178,
+          alt: null,
+        },
+      },
+      waypoints: [
+        {
+          lat: -37.780306410901325,
+          lng: 144.95293953805296,
+        },
+      ],
+    },
+    {
+      holeNum: 7,
+      tees: {
+        tee: {
+          pos: {
+            lat: -37.780114064477,
+            lng: 144.95110308973,
+            alt: null,
+          },
+          nominalDistance: 321,
+          par: 4,
+        },
+      },
+      pins: {
+        pin: {
+          lat: -37.77765318219739,
+          lng: 144.94988796557473,
+          alt: null,
+        },
+      },
+      green: {
+        back: {
+          lat: -37.77752917990108,
+          lng: 144.9498392086975,
+          alt: null,
+        },
+        front: {
+          lat: -37.777799209234104,
+          lng: 144.94992615467177,
+          alt: null,
+        },
+      },
+      waypoints: [
+        {
+          lat: -37.77811322118186,
+          lng: 144.95011870189379,
+        },
+      ],
+    },
+    {
+      holeNum: 8,
+      tees: {
+        tee: {
+          pos: {
+            lat: -37.77738570228341,
+            lng: 144.94943126198015,
+            alt: null,
+          },
+          nominalDistance: 410,
+          par: 5,
+        },
+      },
+      pins: {
+        pin: {
+          lat: -37.780385369280005,
+          lng: 144.9509452569777,
+          alt: null,
+        },
+      },
+      green: {
+        back: {
+          lat: -37.78045561227847,
+          lng: 144.95102676189472,
+          alt: null,
+        },
+        front: {
+          lat: -37.78030157556974,
+          lng: 144.95087056903262,
+          alt: null,
+        },
+      },
+      waypoints: [
+        {
+          lat: -37.77949287607283,
+          lng: 144.94987509552797,
+        },
+      ],
+    },
+    {
+      holeNum: 9,
+      tees: {
+        tee: {
+          pos: {
+            lat: -37.78228015084678,
+            lng: 144.95426010306494,
+            alt: null,
+          },
+          nominalDistance: 293,
+          par: 4,
+        },
+      },
+      pins: {
+        pin: {
+          lat: -37.78380343425557,
+          lng: 144.95655740085658,
+          alt: null,
+        },
+      },
+      green: {
+        back: {
+          lat: -37.78388998298506,
+          lng: 144.95663079229575,
+          alt: null,
+        },
+        front: {
+          lat: -37.78371600408206,
+          lng: 144.95648934012445,
+          alt: null,
+        },
+      },
+      waypoints: [
+        {
+          lat: -37.783313752936245,
+          lng: 144.95617679459008,
+        },
+      ],
+    },
+  ],
+};
---
+++ src/data/courses/au_vic_melbourne_westgate.ts
@@ -0,0 +1,358 @@
+import { CourseDef } from "model/Course";
+
+export const auVicMelbourneWestgate: CourseDef = {
+  courseName: "Westgate Golf Club",
+  holes: [
+    {
+      holeNum: 1,
+      tees: {
+        tee: {
+          pos: {
+            lat: -37.82661280238981,
+            lng: 144.88605054780535,
+            alt: null,
+          },
+          nominalDistance: 103,
+          par: 3,
+        },
+      },
+      pins: {
+        pin: {
+          lat: -37.825786518884016,
+          lng: 144.8862976994324,
+          alt: null,
+        },
+      },
+      green: {
+        back: {
+          lat: -37.82572931609999,
+          lng: 144.886315133791,
+          alt: null,
+        },
+        front: {
+          lat: -37.82584625796043,
+          lng: 144.88628147417322,
+          alt: null,
+        },
+      },
+      waypoints: [
+        {
+          lat: -37.82661280238981,
+          lng: 144.88605054780535,
+        },
+      ],
+    },
+    {
+      holeNum: 2,
+      tees: {
+        tee: {
+          pos: {
+            lat: -37.82459638704781,
+            lng: 144.88659456046312,
+            alt: null,
+          },
+          nominalDistance: 320,
+          par: 4,
+        },
+      },
+      pins: {
+        pin: {
+          lat: -37.822038049938364,
+          lng: 144.88737010671522,
+          alt: null,
+        },
+      },
+      green: {
+        back: {
+          lat: -37.82192575724671,
+          lng: 144.88742777420904,
+          alt: null,
+        },
+        front: {
+          lat: -37.82215352054689,
+          lng: 144.8873218269529,
+          alt: null,
+        },
+      },
+      waypoints: [
+        {
+          lat: -37.822700166483855,
+          lng: 144.88708004029482,
+        },
+      ],
+    },
+    {
+      holeNum: 3,
+      tees: {
+        tee: {
+          pos: {
+            lat: -37.82222821394611,
+            lng: 144.88665443374194,
+            alt: null,
+          },
+          nominalDistance: 299,
+          par: 4,
+        },
+      },
+      pins: {
+        pin: {
+          lat: -37.82457464170446,
+          lng: 144.88574421209012,
+          alt: null,
+        },
+      },
+      green: {
+        back: {
+          lat: -37.82464667595973,
+          lng: 144.88569727343236,
+          alt: null,
+        },
+        front: {
+          lat: -37.82450154805004,
+          lng: 144.8857938329569,
+          alt: null,
+        },
+      },
+      waypoints: [
+        {
+          lat: -37.82420919332817,
+          lng: 144.8859811992792,
+        },
+      ],
+    },
+    {
+      holeNum: 4,
+      tees: {
+        tee: {
+          pos: {
+            lat: -37.82446211866105,
+            lng: 144.8851463877117,
+            alt: null,
+          },
+          nominalDistance: 465,
+          par: 5,
+        },
+      },
+      pins: {
+        pin: {
+          lat: -37.82159521635301,
+          lng: 144.88666239286476,
+          alt: null,
+        },
+      },
+      green: {
+        back: {
+          lat: -37.82152741660566,
+          lng: 144.88669457937294,
+          alt: null,
+        },
+        front: {
+          lat: -37.82167572847209,
+          lng: 144.8866328885656,
+          alt: null,
+        },
+      },
+      waypoints: [
+        {
+          lat: -37.82271285986513,
+          lng: 144.88622078099127,
+        },
+      ],
+    },
+    {
+      holeNum: 5,
+      tees: {
+        tee: {
+          pos: {
+            lat: -37.82112751074862,
+            lng: 144.88597756416394,
+            alt: null,
+          },
+          nominalDistance: 166,
+          par: 3,
+        },
+      },
+      pins: {
+        pin: {
+          lat: -37.82244322881131,
+          lng: 144.88550722529916,
+          alt: null,
+        },
+      },
+      green: {
+        back: {
+          lat: -37.82249513735436,
+          lng: 144.88548844983606,
+          alt: null,
+        },
+        front: {
+          lat: -37.8223796672803,
+          lng: 144.8855340473893,
+          alt: null,
+        },
+      },
+      waypoints: [
+        {
+          lat: -37.82112751074862,
+          lng: 144.88597756416394,
+        },
+      ],
+    },
+    {
+      holeNum: 6,
+      tees: {
+        tee: {
+          pos: {
+            lat: -37.82320120415834,
+            lng: 144.88525289177664,
+            alt: null,
+          },
+          nominalDistance: 232,
+          par: 4,
+        },
+      },
+      pins: {
+        pin: {
+          lat: -37.821512569550556,
+          lng: 144.88637444344673,
+          alt: null,
+        },
+      },
+      green: {
+        back: {
+          lat: -37.821436294745006,
+          lng: 144.8864213821045,
+          alt: null,
+        },
+        front: {
+          lat: -37.82158990364793,
+          lng: 144.8863342103115,
+          alt: null,
+        },
+      },
+      waypoints: [
+        {
+          lat: -37.82188972028326,
+          lng: 144.88616886615523,
+        },
+      ],
+    },
+    {
+      holeNum: 7,
+      tees: {
+        tee: {
+          pos: {
+            lat: -37.82204543924143,
+            lng: 144.8867048312377,
+            alt: null,
+          },
+          nominalDistance: 126,
+          par: 3,
+        },
+      },
+      pins: {
+        pin: {
+          lat: -37.82127261438974,
+          lng: 144.8875849852377,
+          alt: null,
+        },
+      },
+      green: {
+        back: {
+          lat: -37.82124295187828,
+          lng: 144.88763192389547,
+          alt: null,
+        },
+        front: {
+          lat: -37.821319226883645,
+          lng: 144.88753536437093,
+          alt: null,
+        },
+      },
+      waypoints: [
+        {
+          lat: -37.82204543924143,
+          lng: 144.8867061723422,
+        },
+      ],
+    },
+    {
+      holeNum: 8,
+      tees: {
+        tee: {
+          pos: {
+            lat: -37.822029541414295,
+            lng: 144.8878496583566,
+            alt: null,
+          },
+          nominalDistance: 324,
+          par: 4,
+        },
+      },
+      pins: {
+        pin: {
+          lat: -37.824612205050435,
+          lng: 144.88707220683605,
+          alt: null,
+        },
+      },
+      green: {
+        back: {
+          lat: -37.824685298595256,
+          lng: 144.88706684241802,
+          alt: null,
+        },
+        front: {
+          lat: -37.824517924863976,
+          lng: 144.88708830009014,
+          alt: null,
+        },
+      },
+      waypoints: [
+        {
+          lat: -37.824002051441234,
+          lng: 144.88716033063977,
+        },
+      ],
+    },
+    {
+      holeNum: 9,
+      tees: {
+        tee: {
+          pos: {
+            lat: -37.82582521764183,
+            lng: 144.88685360200466,
+            alt: null,
+          },
+          nominalDistance: 122,
+          par: 3,
+        },
+      },
+      pins: {
+        pin: {
+          lat: -37.82653113687751,
+          lng: 144.8865299245082,
+          alt: null,
+        },
+      },
+      green: {
+        back: {
+          lat: -37.826606347119025,
+          lng: 144.8864910324775,
+          alt: null,
+        },
+        front: {
+          lat: -37.826444274249674,
+          lng: 144.88657686316597,
+          alt: null,
+        },
+      },
+      waypoints: [
+        {
+          lat: -37.825600031728634,
+          lng: 144.88700428616391,
+        },
+      ],
+    },
+  ],
+};
