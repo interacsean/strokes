d00f8a206e739ae77413f8e7b0c2b799893f2244
s720788 <sean.hasselback@iag.com.au>
2024-12-27 09:21:17 +1100

    chore: Move default Modal close button position

--- src/presenters/components/Modal/Modal.tsx
+++ src/presenters/components/Modal/Modal.tsx
@@ -4,18 +4,21 @@ import { ReactNode } from "react";
 
 type ModalProps = {
   children: ReactNode;
+  closeButtonPos?: "tl" | "tr",
   onClose: () => void;
 };
 
-export function Modal({ children, onClose }: ModalProps) {
+export function Modal({ children, onClose, closeButtonPos }: ModalProps) {
+  const usedCloseButtonPos = closeButtonPos || "tr";
   return (
-    <Flex flexDir={"column"} alignItems={"stretch"} columnGap={2}>
+    <Flex flexDir={"column"} alignItems={"stretch"} columnGap={2} style={{ maxHeight: "100%", overflowY: "auto" }}>
       <IconButton
         aria-label="close"
         variant="ghost"
         position="absolute"
         top={1}
-        left={1}
+        left={usedCloseButtonPos === "tl" ? 1 : undefined}
+        right={usedCloseButtonPos === "tr" ? 1 : undefined}
         zIndex={10}
         onClick={onClose}
       >
